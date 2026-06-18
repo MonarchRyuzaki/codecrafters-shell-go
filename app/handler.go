418beb98e203b2446241d815654ce251df3e15ea
Shivam Ganguly <114910769+MonarchRyuzaki@users.noreply.github.com>
2026-06-18 12:37:48 +0000

    codecrafters submit [skip ci]

--- app/handler.go
+++ app/handler.go
@@ -5,10 +5,18 @@ import (
 	"fmt"
 )
 
+var builtinCommands = map[string]bool{
+	"echo" : true,
+	"exit" : true,
+	"type" : true,
+}
+
 func Handler(command string, args []string) string {
 	switch command {
 	case "echo":
 		return handleEcho(args)
+	case "type":
+		return handleType(args)
 	default:
 		return fmt.Sprintf("%v: command not found", command)
 	}
@@ -16,4 +24,12 @@ func Handler(command string, args []string) string {
 
 func handleEcho(args []string) string {
 	return strings.Join(args, " ")
+}
+
+func handleType(args []string) string {
+	_, ok := builtinCommands[args[0]]
+	if ok {
+		return fmt.Sprintf("%v is a shell builtin", args[0])
+	}
+	return fmt.Sprintf("%v: not found", args[0])
 }
